ccde69b8ed16ce77d841fa1e8afe6267ab1588dd
Harsimran Singh <91652361+harsimran612@users.noreply.github.com>
2022-03-22 13:21:31 -0700

    the folder structure updated

--- src/backend/routes/userRoutes.js
+++ src/backend/routes/userRoutes.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const req = require("express");
 const { route } = require("express/lib/application");
+
 const {registerUser} = require("../controllers/userControllers");
+
 
 const router = express.Router();
 
-router.route('/signup').post(registerUser);
-router.route('/login');
+router.use('signup',registerUser);
+router.use('signin', signinRouter);
+router.use('/listings', listingRouter);
+router.use('/settings', settingsRouter);
+router.use('/aboutus', aboutusRouter);
+router.use('/contactus', contactusRouter);
+router.use('/request', requestRouter);
 
 
 module.exports = router;
