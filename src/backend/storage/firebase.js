6d63b8aa3471ef6637e3dec3e0b5f28d41efb8e9
Nikhil <nikhil4.deswal@gmail.com>
2022-03-29 02:55:03 +0530

    Adding bucket url for firestore storage access

--- src/backend/storage/firebase.js
+++ src/backend/storage/firebase.js
@@ -3,12 +3,12 @@ const { initializeApp } = require("firebase/app");
 const {getStorage, ref, uploadBytes, getDownloadURL} = require("firebase/storage");
 
 const log = require("../log");
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app, firebaseConfig.bucketUrl);
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const storage = getStorage();
-
 const uploadFile = (file, next) => {
+   
     const storageReference = ref(storage, getUniqueFileName(file.originalname))
     return uploadBytes(storageReference, file.buffer).then(async (snapshot)=>{
         return getDownloadURL(snapshot.ref);
