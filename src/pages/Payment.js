794274642adcec755f1478a3d4c9f1ecc0ba88dc
Harsimran Singh <91652361+harsimran612@users.noreply.github.com>
2022-04-03 21:12:22 -0700

    updates to payment page

--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -18,6 +18,54 @@ const Payment = props => {
                 </ul>
             </div>
         </div>
+
+        <form className="payment-form">
+            <p>Saved Cards:</p>
+
+            <div>
+                <div>
+                    <img src="https://img.icons8.com/color/48/000000/mastercard-logo.png" alt="" />
+                </div>
+                <div>
+                    <input type="text" placeholder="**** **** **** 3193"/>
+                    <div className="card1"> 
+                        <a href="#">Remove Card</a>
+                    </div>
+                </div>
+
+                <p>Add New Card:</p>
+
+                <div>
+
+                    <div>
+                        <div>
+                            <input type="text" placeholder="Card name" />
+                        </div>
+                    </div>
+
+                    <div>
+                        <div>
+                            <div>
+                                <div>
+                                    <input type="text" placeholder="Card number" />
+                                </div>
+                            </div>
+                        </div>
+
+                        <div>
+                            <input type="text" placeholder="Exp. date"/>
+                        </div>
+
+                        <div>
+                            <input type="text" placeholder="CVV" />
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+            <button>Add Card</button>
+        </form>
     </div>
     )};
 
