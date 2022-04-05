664277eb71e051f04796d26cb9f6bc273b8a202f
Himani Gupta <hgupta03@mylangara.ca>
2022-04-05 05:29:04 -0700

    cors

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-// app.use(cors()) // Use this after the variable declaration
+app.use(cors()) // Use this after the variable declaration
 
 // Serve static files from the React frontend app
 //if(process.env.NODE_ENV){
