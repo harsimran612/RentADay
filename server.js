b4f89ae4b161128b41282508df5b669789423648
Himani Gupta <hgupta03@mylangara.ca>
2022-04-05 04:17:53 -0700

    final delpoy - final

--- server.js
+++ server.js
@@ -26,8 +26,8 @@ app.use(function(req, res, next) {
 if(process.env.NODE_ENV){
   app.use(express.static('frontend/build'))
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname + 'frontend/build', 'index.html'))
+  app.get('/*', (req, res) => {
+    res.sendFile(path.resolve(__dirname + '/frontend/build', 'index.html'))
   })
 }
 
