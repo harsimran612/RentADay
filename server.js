c657ec897457749413d6bd1cfcde630f094759ad
Himani Gupta <hgupta03@mylangara.ca>
2022-04-05 03:30:18 -0700

    deploy-5

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(function(req, res, next) {
 // Serve static files from the React frontend app
 app.use(express.static(path.join(__dirname, 'build')))
 
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + 'build', 'index.html'))
 })
 
