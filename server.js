44ba5e9eeebc6f06371d8f35af2995801c195aba
Himani Gupta <hgupta03@mylangara.ca>
2022-04-05 03:43:23 -0700

    deploy-7

--- server.js
+++ server.js
@@ -23,10 +23,10 @@ app.use(function(req, res, next) {
 // app.use(cors()) // Use this after the variable declaration
 
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, 'build')))
+app.use(express.static(path.join(__dirname, 'frontend/build')))
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + 'build', 'index.html'))
+  res.sendFile(path.join(__dirname + 'frontend/build', 'index.html'))
 })
 
 app.use(appRouter);
